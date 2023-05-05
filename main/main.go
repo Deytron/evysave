000295fade716be2feacf4d9a555ab0fd4a3da39
Deytron <romain.ranaivoson@ynov.com>
2023-05-05 11:29:18 +0200

    very start

---
+++ main/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+
+	"github.com/therecipe/qt/widgets"
+)
+
+func main() {
+	app := widgets.NewQApplication(len(os.Args), os.Args)
+
+	widget := widgets.NewQWidget(nil, 0)
+	widget.SetWindowTitle("My Window")
+
+	button := widgets.NewQPushButton2("My Button", nil)
+	button.ConnectClicked(func(checked bool) {
+		widget.SetWindowTitle("Button clicked!")
+	})
+
+	layout := widgets.NewQVBoxLayout2(widget)
+	layout.AddWidget(button, 0, 0)
+
+	widget.SetLayout(layout)
+
+	widget.Show()
+
+	app.Exec()
+}
